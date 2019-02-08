7b9e1159e8dd710abedd528f97e3cfbff42b5500
Marcelo Lino <marcelo.lino@br.experian.com>
2019-02-08 21:45:56 -0200

    version

--- elastic_apm_asgi/__init__.py
+++ elastic_apm_asgi/__init__.py
@@ -1,4 +1,5 @@
 from elastic_apm_asgi.middleware import APMMiddleware
+from version import __version__
 
-__version__ = '0.0.1'
+version = __version__
 __all__ = ['APMMiddleware']
--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def get_version(package):
     Return package version as listed in `__version__` in `init.py`.
     """
     init_py = open(os.path.join(package, '__init__.py')).read()
-    return re.search("__version__ = ['\"]([^'\"]+)['\"]", init_py).group(1)
+    return re.search("version = ['\"]([^'\"]+)['\"]", init_py).group(1)
 
 
 def get_long_description():
